e3b586aa0cf8553b62d37633b501c1e9f43eaa6e
arun-kushwaha04 <arunkushwaha89765@gmail.com>
2021-08-03 21:38:11 +0530

    Added react router dom and context api

---
+++ src/context.js
@@ -0,0 +1,26 @@
+import React, { useContext, useState } from "react";
+
+const AppContext = React.createContext();
+
+const AppProvider = ({ children }) => {
+  const [firstNumber, setFirstNumber] = useState(0);
+  const [secondNumber, setSecondNumber] = useState(0);
+
+  return (
+    <AppContext.Provider
+      value={{
+        firstNumber,
+        setFirstNumber,
+        secondNumber,
+        setSecondNumber,
+      }}>
+      {children}
+    </AppContext.Provider>
+  );
+};
+
+export const UseGlobalContext = () => {
+  return useContext(AppContext);
+};
+
+export { AppProvider };
---
+++ src/home.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const Home = () => {
+  return (
+    <section className="home">
+      <h1>Hello world from home</h1>
+    </section>
+  );
+};
