60299026c676c32c3041de152abad363f7e4bfa3
arun-kushwaha04 <arunkushwaha89765@gmail.com>
2021-08-03 21:57:41 +0530

    Added Particle.js

--- src/home.js
+++ src/home.js
@@ -1,9 +1,11 @@
 import React from "react";
+import Particles from "react-particles-js";
+import { particleConfig } from "./particlesjs-config";
 
 export const Home = () => {
   return (
     <section className="home">
-      <h1>Hello world from home</h1>
+      <Particles params={particleConfig} />
     </section>
   );
 };
